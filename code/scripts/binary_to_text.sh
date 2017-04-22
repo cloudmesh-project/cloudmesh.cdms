3d8f202cc9731aa2762f8e5cee513ab5c06bb9fb
Scott McClary <scmcclar@indiana.edu>
2017-04-22 09:10:13 -0400

    chg:usr: modifying location of CDMS Data

--- code/scripts/binary_to_text.sh
+++ code/scripts/binary_to_text.sh
@@ -2,7 +2,7 @@
 source /home/cc/intel/bin/compilervars.sh intel64
 cdms_scripts=/home/cc/cloudmesh.cdms-master/code/scripts
 ifort $cdms_scripts/binary_to_text.f90 -o $cdms_scripts/binary_to_text
-for filename in /home/cc/cloudmesh.cdms-master/data/*.dat; do
+for filename in /home/cc/data/*.dat; do
     filename_new="${filename%.*}".txt
     $cdms_scripts/binary_to_text < $filename > $filename_new
 done
